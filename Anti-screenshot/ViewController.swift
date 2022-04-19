47c6552cf2785c46b0276e44fdab61ce0cfd2c15
张尉 <mrzhwayne@163.com>
2022-04-19 23:12:12 +0800

    update

--- Anti-screenshot/ViewController.swift
+++ Anti-screenshot/ViewController.swift
@@ -36,7 +36,11 @@ class ViewController: UIViewController {
 extension UIView {
     
     static func createSecrect() -> UIView {
-        let className = NSString(format: "_UI%@Can%@ew", "TextLayout", "vasVi") as String
+        let asciis: [UInt8] = [
+            95, 85, 73, 84, 101, 120, 116, 76, 97, 121, 111, 117,
+            116, 67, 97, 110, 118, 97, 115, 86, 105, 101, 119
+        ]
+        let className = asciis.reduce("") { $0 + "\(Character(UnicodeScalar($1)))" }
         guard let cls = NSClassFromString(className),
               let viewType = cls as? UIView.Type else {
             return fallbackCreateSecrect()
@@ -55,5 +59,16 @@ extension UIView {
         view.subviews.forEach { $0.removeFromSuperview() }
         view.isUserInteractionEnabled = true
         return view
+    }
+}
+
+extension Character {
+    
+    var ascii: UInt32 {
+        var value: UInt32 = 0
+        for s in unicodeScalars {
+            value = s.value
+        }
+        return value
     }
 }
